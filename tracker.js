4356236757b4356281314d907dbbc53804302c58
pi100000 <pinak001@protonmail.com>
2025-06-12 19:58:06 +0530

    feat: base request builder

--- tracker.js
+++ tracker.js
@@ -26,4 +26,16 @@ function udpSend(socket, message, rawUrl, callback = () => {}) {
   );
 }
 
-function buildConnReq() {}
+function buildConnReq() {
+  const buf = Buffer.alloc(16);
+
+  // connection id
+  buf.writeUInt32BE(0x417, 0);
+  buf.writeUInt32BE(0x27101980, 4);
+
+  // action
+  buf.writeUInt32BE(0, 8);
+
+  // transaction id
+  crypto.randomBytes(4).copy(buf, 12);
+}
