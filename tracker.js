6b565dc102d4bd29c59f85fe85c0a1e79c55da3d
pi100000 <pinak001@protonmail.com>
2025-06-25 23:31:48 +0530

    feat: building announce request

--- tracker.js
+++ tracker.js
@@ -1,5 +1,6 @@
 const dgram = require("dgram");
 const url = require("url").parse;
+const util = require("./util");
 
 module.exports.getPeers = (torrent, callback) => {
   const socket = dgram.createSocket("udp4");
@@ -46,4 +47,49 @@ function parseConnResp(resp) {
     transactionId: resp.readUInt32BE(4),
     connectionId: resp.slice(8),
   };
+}
+
+function buildAnnounceReq(connId, torrent, port = 6881) {
+  const buf = Buffer.allocUnsafe(98);
+
+  // connection id
+  connId.copy(buf, 0);
+
+  // action
+  buf.writeUInt32BE(1, 8);
+
+  // transaction id
+  crypto.randomBytes(4).copy(buf, 12);
+
+  // info hash
+  // todo
+
+  // peerId
+  util.genId().copy(buf, 36);
+
+  // downloaded
+  Buffer.alloc(8).copy(buf, 56);
+
+  // left
+  // todo
+
+  // uploaded
+  Buffer.alloc(8).copy(buf, 72);
+
+  // event
+  buf.writeUInt32BE(0, 80);
+
+  // ip address
+  buf.writeUInt32BE(0, 80);
+
+  // key
+  crypto.randomBytes(4).copy(buf, 88);
+
+  // num want
+  buf.writeInt32BE(-1, 92);
+
+  // port
+  buf.writeUInt16BE(port, 96);
+
+  return buf;
 }
