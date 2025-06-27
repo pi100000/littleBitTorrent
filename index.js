7c57ff1d57d8d810aed0ccb2fe6fbbe3b6a20e68
pi100000 <pinak001@protonmail.com>
2025-06-27 23:51:11 +0530

    fix: func separation

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const fs = require("fs");
 const bencodeModule = await import("bencode");
 const bencode = bencodeModule.default;
 const tracker = require("./tracker");
+const torrentParser = require("./torrent-parser");
 
-const torrentFile = bencode.decode(
-  fs.readFileSync("manjaro-xfce-24.2.1-241216-linux612.iso.torrent")
+const torrent = torrentParser.open(
+  "manjaro-xfce-24.2.1-241216-linux612.iso.torrent"
 );
 
 tracker.getPeers(torrent, (peers) => {
