120ad94f9afb88f7aaa7b6f147ef066ff7a76aaa
pi100000 <pinak001@protonmail.com>
2025-06-29 23:57:03 +0530

    feat: info hash func

--- torrent-parser.js
+++ torrent-parser.js
@@ -2,7 +2,13 @@
 
 const fs = require("fs");
 const bencode = require("bencode");
+const crypto = require("crypto");
 
 module.exports.open = (filepath) => {
   return bencode.decode(fs.readFileSync(filepath));
+};
+
+module.exports.infoHash = (torrent) => {
+  const info = bencode.encode(torrent.info);
+  return crypto.createHash("sha1").update(info).digest();
 };
